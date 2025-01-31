6264d8ab4a2d1885db539c9653e039b502c53339
jaeyeop.shim <elapid0212@naver.com>
2025-01-31 16:32:53 +0900

    20250131 ㅂㅓ튼 컴포넌트 추가

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+
+
+const Button = ({text, onClick}) => {
+    return (
+        <button onClick={onClick} className={"Button"}> {/*type에 따라 버튼 색을 다르게 적용하기 위함*/}
+            {text}
+        </button>
+    )
+}
+
+export default Button;
